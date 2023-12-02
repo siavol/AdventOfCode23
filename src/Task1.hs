3468b980ffd6a906c08b1c914472f631f3a119aa
Ilya Shestakov <siavol@gmail.com>
2023-12-02 19:47:06 +0200

    Squashed commit of the following:
    commit 1b791e692cff7cfc94deecdce587f48abaa5e852
    Author: Ilya Shestakov <siavol@gmail.com>
    Date:   Sat Dec 2 19:45:39 2023 +0200
    
        Correct answer for part 2
    
    commit 7d8b3f0ec5f7e84baa7ab3f40d8d56612427c4cd
    Author: Ilya Shestakov <siavol@gmail.com>
    Date:   Sat Dec 2 19:24:51 2023 +0200
    
        Remove HUnit dependency
    
    commit 48c53307434604102335c9964267402d3050d0cb
    Author: Ilya Shestakov <siavol@gmail.com>
    Date:   Sat Dec 2 19:23:58 2023 +0200
    
        try part 2
    
    commit 50d58ff103fb6448dc64c9c7843e515045653ef2
    Author: Ilya Shestakov <siavol@gmail.com>
    Date:   Sat Dec 2 15:31:13 2023 +0200
    
        solve task 1 part 1
    
    commit fae79349960d4d2b4e1e05e2232f823ba64fe1ff
    Author: Ilya Shestakov <siavol@gmail.com>
    Date:   Sat Dec 2 15:13:47 2023 +0200
    
        Add functions to extract digits from a string

--- src/Task1.hs
+++ src/Task1.hs
@@ -1,13 +1,16 @@
 module Task1 where
 
-import Data.List (sort)
+import Data.List (sort, isPrefixOf)
+import Data.Char (digitToInt)
 
 runTask :: IO ()
 runTask = do
   print "=== Advent Code 1 ==="
   input <- readInput "./inputs/task-1.txt"
-  -- print input
-  print "hello world"
+  print $ calibrationSum input
+  print $ calibrationSumWithReplace input
+  -- print "====================="
+  -- print $ debugOutput input
 
 
 -- reads input from file
@@ -15,3 +18,62 @@ readInput :: FilePath -> IO [String]
 readInput filename = do
   contents <- readFile filename
   return (lines contents)
+
+isDigit :: Char -> Bool
+isDigit c = c `elem` ['0'..'9']
+
+-- returns digits from string like "1abc2" -> [1, 2]
+getDigits :: String -> [Int]
+getDigits = map digitToInt . filter isDigit
+
+-- returns calibration value
+calibrationValue :: String -> Int
+calibrationValue s = a * 10 + b
+  where
+    digits = getDigits s
+    a = head digits
+    b = last digits
+
+-- returns a maybe tuple of (digit, length) when string starts with digit or letter
+-- like "1abc" -> (1, 1)
+-- like "oneabc" -> (1, 3)
+startsWithDigitOrLetter :: String -> Maybe (Int, Int)
+startsWithDigitOrLetter s
+  | isDigit (head s) = Just (digitToInt (head s), 1)
+  | isPrefixOf "one" s = Just (1, 3)
+  | isPrefixOf "two" s = Just (2, 3)
+  | isPrefixOf "three" s = Just (3, 5)
+  | isPrefixOf "four" s = Just (4, 4)
+  | isPrefixOf "five" s = Just (5, 4)
+  | isPrefixOf "six" s = Just (6, 3)
+  | isPrefixOf "seven" s = Just (7, 5)
+  | isPrefixOf "eight" s = Just (8, 5)
+  | isPrefixOf "nine" s = Just (9, 4)
+  | otherwise = Nothing
+
+-- returns digits from string
+getDigitsWithLetters :: String -> [Int]
+getDigitsWithLetters str = go str []
+  where
+    go :: String -> [Int] -> [Int]
+    go s acc
+      | null s = acc
+      | Just (i, len) <- startsWithDigitOrLetter s = go (tail s) (acc ++ [i])
+      | otherwise = go (tail s) acc
+
+calibrationValueWithLetters :: String -> Int
+calibrationValueWithLetters s = a * 10 + b
+  where
+    digits = getDigitsWithLetters s
+    a = head digits
+    b = last digits
+
+-- returns sum of calibration values
+calibrationSum :: [String] -> Int
+calibrationSum = sum . map calibrationValue
+
+calibrationSumWithReplace :: [String] -> Int
+calibrationSumWithReplace = sum . map calibrationValueWithLetters
+
+debugOutput :: [String] -> [(Int, String)]
+debugOutput = map (\s -> (calibrationValueWithLetters s, s))
--- tests/Task1.test.hs
+++ tests/Task1.test.hs
@@ -1,20 +1,66 @@
 import Test.HUnit
 import qualified System.Exit as Exit
-import Task1 (sumAll, mapToInt)
+import Task1 (
+    getDigits,
+    isDigit,
+    calibrationValue,
+    calibrationSum,
+    calibrationSumWithReplace,
+    getDigitsWithLetters)
 
-testSumAll :: Test
-testSumAll = TestCase
-    $ assertEqual "for (sumAll [[1,2,3],[4,5,6],[7,8,9]])"
-    [6, 15, 24] (sumAll [[1,2,3],[4,5,6],[7,8,9]])
+testIsDigit_1 :: Test
+testIsDigit_1 = TestCase
+    $ assertEqual "for (isDigit '1')" True (isDigit '1')
 
-testMapToInt :: Test
-testMapToInt = TestCase
-    $ assertEqual "for (mapToInt [[\"1\",\"2\",\"3\"],[\"4\",\"5\",\"6\"]])"
-    [[1,2,3], [4,5,6]] (mapToInt [["1","2","3"],["4","5","6"]])
+testIsDigit_a :: Test
+testIsDigit_a = TestCase
+    $ assertEqual "for (isDigit 'a')" False (isDigit 'a')
+
+testGetDigits :: Test
+testGetDigits = TestCase
+    $ assertEqual "for (getDigits '1abc2')"
+    [1, 2] (getDigits "1abc2")
+
+testCalibrationValue :: Test
+testCalibrationValue = TestCase
+    $ assertEqual "for (calibrationValue '1abc2')"
+    12 (calibrationValue "1abc2")
+
+testCalibrationSum :: Test
+testCalibrationSum = TestCase
+    $ assertEqual "for (calibrationSum <sample input>)"
+    142 (calibrationSum ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"])
+
+testGetDigitsWithLetters :: Test
+testGetDigitsWithLetters = TestCase
+    $ assertEqual "for (digitIndexes 'zoneight234')"
+    [1, 2, 3, 4] (getDigitsWithLetters "zoneight234")
+
+testGetDigitsWithLetters2 :: Test
+testGetDigitsWithLetters2 = TestCase
+    $ assertEqual "for (digitIndexes 'ltgeightwothree5ccxbhssxrsbj')"
+    [8, 2, 3, 5] (getDigitsWithLetters "ltgeightwothree5ccxbhssxrsbj")
+
+testCalibrationSumWithReplace :: Test
+testCalibrationSumWithReplace = TestCase
+    $ assertEqual "for (calibrationSumWithReplace <sample input>)"
+    281 (calibrationSumWithReplace ["two1nine",
+                                    "eightwothree",
+                                    "abcone2threexyz",
+                                    "xtwone3four",
+                                    "4nineeightseven2",
+                                    "zoneight234",
+                                    "7pqrstsixteen"])
 
 tests = TestList [
-    TestLabel "Test sumAll" testSumAll,
-    TestLabel "Test mapToInt" testMapToInt
+      TestLabel "Test getDigits" testGetDigits
+    , TestLabel "Test isDigit" testIsDigit_1
+    , TestLabel "Test isDigit" testIsDigit_a
+    , TestLabel "Test calibrationValue" testCalibrationValue
+    , TestLabel "Test calibrationSum" testCalibrationSum
+    , TestLabel "Test testGetDigitsWithLetters" testGetDigitsWithLetters
+    , TestLabel "Test testGetDigitsWithLetters2" testGetDigitsWithLetters2
+    , TestLabel "Test calibrationSumWithReplace" testCalibrationSumWithReplace
     ]
 
 main :: IO Counts
